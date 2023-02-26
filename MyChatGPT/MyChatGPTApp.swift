aad394b1caed1565619e64156fe335270716cff8
dcaric <dcaric@me.com>
2023-02-26 15:05:58 +0100

    Initial Commit

---
+++ MyChatGPT/MyChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyChatGPTApp.swift
+//  MyChatGPT
+//
+//  Created by Dario Caric on 26.02.2023..
+//
+
+import SwiftUI
+
+@main
+struct MyChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
